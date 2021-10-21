f927d3fff4c03f3e1412368c1bd046c1082a558c
NoorMdKhan123 <noormd6400@gmail.com>
2021-10-22 04:08:42 +0530

    Added Part Time wage

--- Program.cs
+++ Program.cs
@@ -8,18 +8,24 @@ namespace EmployeeWageAIApproach
 
         static void Main(string[] args)
         {
-            //constant
+            //constant 
             int is_full_time = 1;
+            int is_part_time = 2;
             int emp_rate_per_hour = 20;
+
             //variable
             int emp_wage = 0;
             int emp_hrs = 0;
 
             Random random = new Random();
-            int empCheck = random.Next(0, 2);
+            int empCheck = random.Next(0, 3);
             if (empCheck == is_full_time)
             {
                 emp_hrs = 8;
+            }
+            else if (empCheck == is_part_time)
+            {
+                emp_hrs = 4;
             }
             else
             {
