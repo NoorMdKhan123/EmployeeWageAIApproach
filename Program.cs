76a89a395193eb0501289e81dc6aea81a1317b83
NoorMdKhan123 <noormd6400@gmail.com>
2021-10-22 04:03:12 +0530

    Employee Atendance

--- Program.cs
+++ Program.cs
@@ -4,9 +4,21 @@ namespace EmployeeWageAIApproach
 {
     class Program
     {
-        static void Main(string[] args)
-        {
-            Console.WriteLine("Welcome to Employee Wage Computation Program");
-        }
+        
+        
+            static void Main(string[] args)
+            {
+                int employee_is_present = 1;
+
+                Random random = new Random();
+                int empCheck = random.Next(0, 2);
+                if (empCheck == employee_is_present)
+                    Console.WriteLine("Employee is Present");
+                else
+                {
+                    Console.WriteLine("employee is absent");
+                }
+            }
+        
     }
 }
