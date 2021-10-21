de737da191a7563c8742c46753ccdf8b3a70c0a8
NoorMdKhan123 <noormd6400@gmail.com>
2021-10-22 04:20:50 +0530

    Applied days and hours conditions

--- Program.cs
+++ Program.cs
@@ -5,10 +5,12 @@ namespace EmployeeWageAIApproach
     class Program
     {
 
+        //constant
         public const int is_full_time = 1;
         public const int is_part_time = 2;
         public const int emp_rate_per_hour = 20;
-        public const int total_working_days = 20;
+        public const int max_working_days = 20;
+        public const int max_working_hours = 100;
         static void Main(string[] args)
         {
 
@@ -16,11 +18,14 @@ namespace EmployeeWageAIApproach
             //variable
             int emp_wage = 0;
             int emp_hrs = 0;
+            int total_emp_hrs = 0;
             int total_emp_wage = 0;
+            int total_woking_days = 0;
 
 
-            for (int day = 1; day <= total_working_days; day++)
+            while (total_woking_days < max_working_days && emp_hrs <= max_working_hours)
             {
+                total_woking_days++;
                 Random random = new Random();
                 int empCheck = random.Next(0, 3);
                 switch (empCheck)
@@ -44,10 +49,11 @@ namespace EmployeeWageAIApproach
 
                 emp_wage = emp_hrs * emp_rate_per_hour;
                 total_emp_wage += emp_wage;
-                Console.WriteLine("Emp Wage for day " + day + " is " + emp_wage);
+                total_emp_hrs += emp_hrs;
+                Console.WriteLine("Emp Wage for day " + total_woking_days + " is " + emp_wage);
             }
-            Console.WriteLine("Emp Wage for whole month is : " + total_emp_wage);
-
+            Console.WriteLine("Emp Wage for whole month is : " + total_emp_wage +
+                " total emp hrs " + total_emp_hrs);
         }
     }
 }
