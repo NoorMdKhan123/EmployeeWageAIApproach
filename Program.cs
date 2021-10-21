7b45e6ca9791f3817f4a9f053aa9444ff15890d2
NoorMdKhan123 <noormd6400@gmail.com>
2021-10-22 04:17:47 +0530

    Calculated wages of whole month

--- Program.cs
+++ Program.cs
@@ -8,6 +8,7 @@ namespace EmployeeWageAIApproach
         public const int is_full_time = 1;
         public const int is_part_time = 2;
         public const int emp_rate_per_hour = 20;
+        public const int total_working_days = 20;
         static void Main(string[] args)
         {
 
@@ -15,31 +16,38 @@ namespace EmployeeWageAIApproach
             //variable
             int emp_wage = 0;
             int emp_hrs = 0;
+            int total_emp_wage = 0;
 
-            Random random = new Random();
-            int empCheck = random.Next(0, 3);
-            switch (empCheck)
+
+            for (int day = 1; day <= total_working_days; day++)
             {
-                case is_full_time:
+                Random random = new Random();
+                int empCheck = random.Next(0, 3);
+                switch (empCheck)
+                {
+                    case is_full_time:
 
-                    emp_hrs = 8;
-                    break;
-
-
-                case is_part_time:
-
-                    emp_hrs = 4;
-                    break;
+                        emp_hrs = 8;
+                        break;
 
 
-                default:
-                    emp_hrs = 0;
-                    break;
+                    case is_part_time:
+
+                        emp_hrs = 4;
+                        break;
+
+
+                    default:
+                        emp_hrs = 0;
+                        break;
+                }
+
+                emp_wage = emp_hrs * emp_rate_per_hour;
+                total_emp_wage += emp_wage;
+                Console.WriteLine("Emp Wage for day " + day + " is " + emp_wage);
             }
-            emp_wage = emp_hrs * emp_rate_per_hour;
-            Console.WriteLine("Emp Wage is : " + emp_wage);
+            Console.WriteLine("Emp Wage for whole month is : " + total_emp_wage);
+
         }
-
-
     }
 }
